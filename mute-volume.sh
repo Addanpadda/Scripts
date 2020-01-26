228cdd7f3f14e5b9b1a2b3800a7bf31505980dbf
Addanpadda <adamherbertsson@gmail.com>
2020-01-26 17:02:13 +0100

    Update

--- mute-volume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pactl set-sink-mute $(pacmd list-sinks | grep "*" | awk '{print $3}') toggle && killall status-bar.sh && /home/adam/Scripts/status-bar.sh &
--- ssd-space.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "$(echo "scale=1;" "$(df /dev/mapper/ssd--volgroup0-lv--root | awk 'NR == 2 {print $4}')/1000000" | bc)gb"
