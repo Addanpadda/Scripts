4f5015366e8130a627fe7142e81edcc7efd4e7d5
Addanpadda <adamherbertsson@gmail.com>
2020-01-26 16:52:47 +0100

    Update

--- volume-lower.sh
+++ volume-lower.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pactl set-sink-volume $(pacmd list-sinks | grep "*" | awk '{print $3}') -2% && killall status-bar.sh && /home/adam/Scripts/status-bar.sh &
+pactl set-sink-volume $(pacmd list-sinks | grep "*" | awk '{print $3}') -2%
+# Dwm
+#killall status-bar.sh && /home/adam/Scripts/status-bar.sh &
