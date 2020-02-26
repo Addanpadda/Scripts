a3f5191098273ceb9d5346c0188161d6c62e36d8
Addanpadda <adamherbertsson@gmail.com>
2020-02-26 22:20:15 +0100

    Added an ESO game launcher script

---
+++ eso.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Start steam if it is not already running
+if [ ! $(ps -ef | grep steam | wc -l) -gt 1 ]; then
+	steam > /dev/null 2>&1 &
+fi
+
+# Run ESO with proton
+STEAM_COMPAT_DATA_PATH="/home/adam/.proton/Elder Scrolls Online" /usr/local/games/steam/steamapps/common/Proton\ 4.2/proton run /home/adam/.proton/Elder\ Scrolls\ Online/pfx/drive_c/Program\ Files\ \(x86\)/Zenimax\ Online/Launcher/Bethesda.net_Launcher.exe -window -steam > /dev/null 2>&1 &
